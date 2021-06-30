978c1ab43ec3a7fac8968bcc862404df87766e09
Cindy Zhao <14973337+xinleizhao@users.noreply.github.com>
2021-06-30 10:53:08 +0800

    Update bps.py

--- bps.py
+++ bps.py
@@ -1,17 +1,24 @@
 from __future__ import division
-from __future__ import print_function import sys import copy import pprint 
+from __future__ import print_function 
+import sys 
+import copy 
+import pprint 
 prev_value = {} 
 prev_time = {} 
 prev_bps = {} 
+
 ''' This function returns the delta of the counter between current and previous values ''' 
 def interface_bytes_to_bps(index_name, direction, bytes_counter, **kwargs): 
     global prev_value
     global prev_time
     global prev_bps
+    
     index_name_direction = index_name + "-" + direction
-    cur_time = kwargs.get('point_time', 0) bytes_counter = int(bytes_counter) 
+    cur_time = kwargs.get('point_time', 0) 
+    bytes_counter = int(bytes_counter) 
     # convert bytes into bits 
-    # bps = (bytes * 8) bits_counter = bytes_counter * 8 
+    # bps = (bytes * 8) 
+    bits_counter = bytes_counter * 8 
     cur_value = bits_counter 
     
     # calculate the time difference 
